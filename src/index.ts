b5f62963314f465fe44166925dba7305292e9852
sl1673495 <949589855@qq.com>
2020-01-25 18:35:37 +0800

    refactor: effect只调用一次

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,38 @@
-import React, { useReducer, useEffect } from 'react';
-import { effect, stop } from '@vue/reactivity';
+import React, { useReducer, useEffect, useRef } from 'react';
+import { effect, stop, ReactiveEffect } from '@vue/reactivity';
 
 const setup = <Props extends {} = {}>(factory: () => React.FC<Props>) => {
   // 执行factory的过程中 会调用用户传入的setup
   // 在其中可以调用@vue/reactivity仓库里的api使用响应式能力
   const FunctionComponent = factory();
 
-  const Wrapped: React.FC<Props> = props => {
+  const EffectWrapper: React.FC<Props> = props => {
     const forceUpdate = useForceUpdate();
 
-    // 将React生成组件的函数包裹在effect中开启观察
-    // 在观察到的值更新以后利用forceUpdate来强制渲染
-    let Component = null as ReturnType<React.FunctionComponent>;
-    const effection = effect(
-      () => {
-        Component = FunctionComponent(props);
-      },
-      {
-        scheduler: forceUpdate,
-      },
-    );
+    // 将React的FunctionComponent函数包裹在effect中执行 以收集依赖
+    // 依赖值更新以后利用forceUpdate来强制重新渲染组件
+    // effect只需要执行一次就够了
+    const effectionRef = useRef<ReactiveEffect>();
+    if (!effectionRef.current) {
+      effectionRef.current = effect(
+        () =>  FunctionComponent(props),
+        { scheduler: forceUpdate, lazy: true },
+      );
+    }
+
+    // 生成组件实例
+    const Component = effectionRef.current()
 
     // 组件卸载后停止观察
     const stopEffect = () => {
-      stop(effection);
+      stop(effectionRef.current!);
     };
     useEffect(() => stopEffect, []);
 
     return Component;
   };
 
-  return Wrapped;
+  return EffectWrapper;
 };
 
 export { setup };
