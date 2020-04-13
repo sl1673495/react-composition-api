ae7a6b71eacbca7172b8ac0180212cdc48a9bbed
ssh <949589855@qq.com>
2020-04-13 17:29:24 +0800

    feat: remove setup api and improve docs

--- demo/counter/store.ts
+++ demo/counter/store.ts
@@ -4,15 +4,8 @@ export const state = reactive({
   count: 0,
 });
 
-const add = () => (state.count += 1);
+export const add = () => (state.count += 1);
 
-export const mutations = {
-  // mutation
-  add,
-};
-
-export const store = {
-  state: reactive(state),
-};
+export const store = reactive(state)
 
 export type Store = typeof store;
